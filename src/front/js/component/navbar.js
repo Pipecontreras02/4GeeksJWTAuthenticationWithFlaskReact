9c75ee51729816710645c3b947842d0cf139109b
Pipecontreras02 <pipecontreras2@gmail.com>
2022-03-30 03:52:53 +0000

    first session of the JWT, priivate missing

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,17 +1,27 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Context } from "../store/appContext";
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
-	return (
+	const { store, actions } = useContext(Context);
+
+;	return (
 		<nav className="navbar navbar-light bg-light">
 			<div className="container">
 				<Link to="/">
 					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
 				</Link>
 				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
+					{!store.token ? 
+						(<Link to="/login">
+						<button className="btn btn-primary">Log in</button>
+						</Link>)
+						:
+						(
+						<button className="btn btn-secondary" onClick = {() => actions.logout()}>Logout</button>
+						)
+					}  
+					
 				</div>
 			</div>
 		</nav>
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,49 +1,104 @@
 const getState = ({ getStore, getActions, setStore }) => {
-	return {
-		store: {
-			message: null,
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
-		},
-		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
+  return {
+    store: {
+      token: null,
+      message: null,
+      demo: [
+        {
+          title: "FIRST",
+          background: "white",
+          initial: "white",
+        },
+        {
+          title: "SECOND",
+          background: "white",
+          initial: "white",
+        },
+      ],
+    },
+    actions: {
+      // Use getActions to call a function within a fuction
+      
+      logout: () => {
+        sessionStorage.removeItem("token");
+        console.log("Loggin Out");
+        setStore({ token: null });
+      },
+      syncTokenFromSessionStorage: () => {
+        const token = sessionStorage.getItem("token");
+        console.log(
+          "application just loaded, synching the session storage token"
+        );
+        if (token && token != "" && token != undefined)
+          setStore({ token: token });
+      },
 
-			getMessage: () => {
-				// fetching data from the backend
-				fetch(process.env.BACKEND_URL + "/api/hello")
-					.then(resp => resp.json())
-					.then(data => setStore({ message: data.message }))
-					.catch(error => console.log("Error loading message from backend", error));
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
+      exampleFunction: () => {
+        getActions().changeColor(0, "green");
+      },
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
+      login: async (email, password) => {
+        const opts = {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email: email,
+            password: password
+          }),
+        };
+        try {
+          const resp = await fetch(
+            "https://3001-4geeksacademy-reactflask-ounqekb4moh.ws-us38.gitpod.io/api/token",
+            opts
+          );
+          if (resp.status !== 200) {
+            alert("there has been an error");
+            return false;
+          }
 
-				//reset the global store
-				setStore({ demo: demo });
-			}
-		}
-	};
+          const data = await resp.json();
+          console.log("this came from the backend", data);
+          sessionStorage.setItem("token", data.access_token);
+          setStore: ({ token: data.access_token });
+          return true;
+        } catch (error) {
+          console.log("there an error while logiining in");
+        }
+      },
+      getMessage: () => {
+        const store = getStore();
+        const opts = {
+          headers: {
+            "authorization": "Bearer " + store.token
+          }
+        }
+
+        // fetching data from the backend
+        fetch("https://3001-4geeksacademy-reactflask-ounqekb4moh.ws-us38.gitpod.io/api/hello", opts)
+          .then((resp) => resp.json())
+          .then((data) => setStore({ message: data.message }))
+          .catch((error) =>
+            console.log("Error loading message from backend", error)
+          );
+      },
+      changeColor: (index, color) => {
+        //get the store
+        const store = getStore();
+
+        //we have to loop the entire demo array to look for the respective index
+        //and change its color
+        const demo = store.demo.map((elm, i) => {
+          if (i === index) elm.background = color;
+          return elm;
+        });
+
+        //reset the global store
+        setStore({ demo: demo });
+      },
+    },
+  };
 };
 
 export default getState;
